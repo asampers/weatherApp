fbbc72302a4dab5e9eb7d46367a0db7113fd00c8
nanarama <annasampers@gmail.com>
2023-06-29 13:47:12 -0500

    Add weatherDataDisplay

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,12 @@
 import "../scss/styles.scss";
 import SearchBar from "./form";
 import SearchResults from "./searchResults";
+import { weatherDisplay } from "./weatherDataDisplay";
 
 const searchbar = SearchBar();
 const display = document.querySelector(".container");
 const citySearchResults = SearchResults.resultsContainer;
 
-display.append(searchbar, citySearchResults);
+display.append(searchbar, citySearchResults, weatherDisplay);
 
 export { searchbar };
---
+++ src/js/weatherDataDisplay.js
@@ -0,0 +1,22 @@
+const WeatherDataDisplay = () => {
+  const container = document.createElement("div");
+
+  return container;
+};
+
+const DisplayContent = () => {
+  const container = document.createElement("div");
+
+  const clear = () => {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  };
+
+  return { container, clear };
+};
+
+const weatherDisplay = WeatherDataDisplay();
+const displayContent = DisplayContent();
+
+export { weatherDisplay, displayContent };
