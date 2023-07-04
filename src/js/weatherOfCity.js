acbe44c11883514ca1b8da1032eeea7de0b49233
nanarama <annasampers@gmail.com>
2023-07-04 15:03:58 -0500

    Style weatherOfCity, add misc-icons

--- src/js/weatherOfCity.js
+++ src/js/weatherOfCity.js
@@ -4,14 +4,20 @@ const WeatherOfCity = () => {
   //Header of Card
   const cityName = document.createElement("div");
   cityName.textContent = "Milwaukee, Wisconsin";
+  cityName.className = "h3";
 
   const dateTime = document.createElement("div");
   dateTime.textContent = "2023-06-29 15:05";
+  dateTime.className = "small text-secondary";
 
-  //Main Temperature Section
+  //Main Section
+  const mainSection = document.createElement("div");
+  mainSection.className = "d-flex flex-column align-items-center";
   const tempSection = document.createElement("div");
+  tempSection.className = "d-flex align-items-center ";
 
   const temp = document.createElement("div");
+  temp.className = "me-4 flex-grow-1 fs-2";
   temp.textContent = "84.2°F";
 
   const condition = document.createElement("div");
@@ -19,44 +25,41 @@ const WeatherOfCity = () => {
 
   const icon = new Image();
   icon.src = new URL("../assets/day/302.png", import.meta.url);
-  console.log(`${import.meta.url}`);
+
+  const highLowTemps = document.createElement("div");
+
   const highTemp = document.createElement("div");
+  highTemp.className = "small";
   highTemp.textContent = "84.2°F";
 
   const lowTemp = document.createElement("div");
+  lowTemp.className = "small";
   lowTemp.textContent = "61.7°F";
 
-  tempSection.append(temp, condition, icon, highTemp, lowTemp);
+  highLowTemps.append(highTemp, lowTemp);
+  tempSection.append(temp, highLowTemps);
+  mainSection.append(icon, tempSection, condition);
 
   //Miscellaneous Weather Section
   const miscWeatherSection = document.createElement("div");
 
-  const feelsLike = document.createElement("div");
-  feelsLike.textContent = "89.2°F";
-
   const humidity = document.createElement("div");
+  humidity.className = "humidity";
   humidity.textContent = "52%";
 
-  const windSpeed = document.createElement("div");
-  windSpeed.textContent = "9.4 mph";
-
-  const windDir = document.createElement("div");
-  windDir.textContent = "ENE";
+  const wind = document.createElement("div");
+  wind.className = "wind"
+  wind.textContent = "9.4 mph ENE";
 
   const sunrise = document.createElement("div");
+  sunrise.className = "sunrise"
   sunrise.textContent = "05:15 AM";
 
   const sunset = document.createElement("div");
+  sunset.className = "sunset"
   sunset.textContent = "08:35 PM";
 
-  miscWeatherSection.append(
-    feelsLike,
-    humidity,
-    windSpeed,
-    windDir,
-    sunrise,
-    sunset
-  );
+  miscWeatherSection.append(humidity, wind, sunrise, sunset);
   //End of Miscellaneous weather section
 
   const update = (data) => {
@@ -68,10 +71,8 @@ const WeatherOfCity = () => {
       icon,
       highTemp,
       lowTemp,
-      feelsLike,
       humidity,
-      windSpeed,
-      windDir,
+      wind,
       sunrise,
       sunset,
     };
@@ -79,7 +80,7 @@ const WeatherOfCity = () => {
     populateWeatherDisplay(cardObjs, data);
   };
 
-  card.append(cityName, dateTime, tempSection, miscWeatherSection);
+  card.append(cityName, dateTime, mainSection, miscWeatherSection);
 
   return { card, update };
 };
@@ -95,10 +96,8 @@ function populateWeatherDisplay(objs, data) {
   });
   objs.highTemp.textContent = `H: ${data.forecast.forecastday[0].day.maxtemp_f}°F`;
   objs.lowTemp.textContent = `L: ${data.forecast.forecastday[0].day.mintemp_f}°F`;
-  objs.feelsLike.textContent = `Feels like: ${data.current.feelslike_f}°F`;
   objs.humidity.textContent = `Humidity: ${data.current.humidity}%`;
-  objs.windSpeed.textContent = `${data.current.wind_mph} mph`;
-  objs.windDir.textContent = `${data.current.wind_dir}`;
+  objs.wind.textContent = `${data.current.wind_mph} mph ${data.current.wind_dir}`;
   objs.sunrise.textContent = `Sunrise: ${data.forecast.forecastday[0].astro.sunrise}`;
   objs.sunset.textContent = `Sunset: ${data.forecast.forecastday[0].astro.sunset}`;
 }
