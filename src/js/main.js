3b2a56d1c56027cbc0fdb79130f41ade5694e84e
nanarama <annasampers@gmail.com>
2023-07-07 17:25:00 -0500

    Preliminary footer with popover

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,13 @@ const citySearchResults = SearchResults.resultsContainer;
 
 display.append(searchbar, citySearchResults, weatherDisplay);
 
+let toggle = document.querySelector(".popup");
+
+function myFunction() {
+  let popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+toggle.addEventListener("click", myFunction);
+
 export { searchbar };
