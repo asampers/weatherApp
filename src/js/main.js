e754ca986484836398c493402e10aa7b7ef61d7b
nanarama <annasampers@gmail.com>
2023-07-11 13:36:11 -0500

    Add title within js

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,15 @@ import SearchResults from "./searchResults";
 import { weatherDisplay } from "./weatherDataDisplay";
 import { addPopupEventListener, footer } from "./footer";
 
+const title = document.createElement("h1");
+title.className = "text-center";
+title.textContent = "What's the Weather?";
+
 const searchbar = SearchBar();
 const display = document.querySelector(".container");
 const citySearchResults = SearchResults.resultsContainer;
 
-display.append(searchbar, citySearchResults, weatherDisplay);
+display.append(title, searchbar, citySearchResults, weatherDisplay);
 display.insertAdjacentHTML("afterend", footer);
 addPopupEventListener();
 
