a5b23d7ca99a5dbb56f2f40bd488155cac65c0ac
nanarama <annasampers@gmail.com>
2023-07-11 12:21:13 -0500

    Fix footer placement

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,16 @@ import "../scss/styles.scss";
 import SearchBar from "./form";
 import SearchResults from "./searchResults";
 import { weatherDisplay } from "./weatherDataDisplay";
+import Footer from "./footer";
 
 const searchbar = SearchBar();
 const display = document.querySelector(".container");
 const citySearchResults = SearchResults.resultsContainer;
 
+let footer = Footer();
+
 display.append(searchbar, citySearchResults, weatherDisplay);
+footer.addFoot();
 
 let toggle = document.querySelector(".popup");
 
