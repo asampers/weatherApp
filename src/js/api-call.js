9bf481608e9b1282e9c69b9a0704b4bdd2b33b36
nanarama <annasampers@gmail.com>
2023-07-11 17:54:56 -0500

    Fix https

--- src/js/api-call.js
+++ src/js/api-call.js
@@ -3,7 +3,7 @@ const apiKey = "3d62ba0524d94ae9877163800232606";
 async function apiCallGenerator(source) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/${source.getSearchTerm()}.json?key=${apiKey}&q=${source.getCity()}&${source.getExtras()}`,
+      `https://api.weatherapi.com/v1/${source.getSearchTerm()}.json?key=${apiKey}&q=${source.getCity()}&${source.getExtras()}`,
       { mode: "cors" }
     );
     const results = await response.json();
