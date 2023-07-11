b3656da0812256ab04e84049c52e6b002eecc015
nanarama <annasampers@gmail.com>
2023-07-11 13:31:51 -0500

    Refactor footer

--- src/js/main.js
+++ src/js/main.js
@@ -3,24 +3,14 @@ import "../scss/styles.scss";
 import SearchBar from "./form";
 import SearchResults from "./searchResults";
 import { weatherDisplay } from "./weatherDataDisplay";
-import Footer from "./footer";
+import { addPopupEventListener, footer } from "./footer";
 
 const searchbar = SearchBar();
 const display = document.querySelector(".container");
 const citySearchResults = SearchResults.resultsContainer;
 
-let footer = Footer();
-
 display.append(searchbar, citySearchResults, weatherDisplay);
-footer.addFoot();
-
-let toggle = document.querySelector(".popup");
-
-function myFunction() {
-  let popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-
-toggle.addEventListener("click", myFunction);
+display.insertAdjacentHTML("afterend", footer);
+addPopupEventListener();
 
 export { searchbar };
